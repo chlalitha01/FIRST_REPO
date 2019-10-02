6ef3fbfc7875ca97f8ca3fd5c8f3817fd7b4767c
chlalitha01 <55826997+chlalitha01@users.noreply.github.com>
2019-10-02 17:06:03 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,17 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-#define gl(a) getline(cin,a)
 #define ll long long int
-#define mp make_pair
-#define pb push_back
-#define MOD 1000000007
-#define pi 3.1415926535897932384
-#define vsort(v) sort(v.begin(),v.end())
-#define max(a,b) ((a)>(b)?(a):(b))
- 
-#define deb(x) cout<< #x <<" "<<x<<endl;
-//std::cout << std::setprecision(12) << std::fixed;
+
 int main() {
 	
 	ll t,n,a,s;
